4b4b346702112f0a53fdb1b132d9d32efd22986a
Matthieu Seynaeve <matthieu.seynaeve@laposte.fr>
2024-04-22 16:49:20 +0200

    Add summaries

--- src/components/ThemeCard/index.js
+++ src/components/ThemeCard/index.js
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 
 export default function ThemeCard({ href, imgUrl, title }) {
     return (
-        <a href={`/docs/${href}`}>
+        <a href={`/docs/category/${href}`}>
             <div className={styles.card}>
                 <img src={`/img/${imgUrl}`} alt="Logo"/>
                 <div className={styles.cardbody}>
